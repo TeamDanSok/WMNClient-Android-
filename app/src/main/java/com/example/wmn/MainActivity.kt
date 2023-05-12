2c12acb2849c9dc0d2a69c1ccc754df9e700b875
suyeun84 <kelsey6225@naver.com>
2023-05-12 15:49:56 +0900

    semi
    semi

--- app/src/main/java/com/example/wmn/MainActivity.kt
+++ app/src/main/java/com/example/wmn/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wmn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.wmn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
+    }
+
+    fun init(){
+        binding.sendButton.setOnClickListener {
+            var message = binding.message.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            binding.message.text.clear()
+            //채팅화면으로 전환시키면 됨
+        }
     }
 }
