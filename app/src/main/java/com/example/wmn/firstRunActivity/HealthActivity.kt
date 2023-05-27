f4623813ac819aec59a4dee7313d826fb36410ca
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2023-05-27 21:11:40 +0900

    firstrunActivity 수정 및 정보 전달 추가

--- app/src/main/java/com/example/wmn/firstRunActivity/HealthActivity.kt
+++ app/src/main/java/com/example/wmn/firstRunActivity/HealthActivity.kt
@@ -14,42 +14,40 @@ import com.example.wmn.databinding.ActivityHealthBinding
 
 class HealthActivity : AppCompatActivity() {
     lateinit var binding : ActivityHealthBinding
+    lateinit var healthInfo : String
+    lateinit var preferredFood : String
+    lateinit var username : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHealthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val healthEdit : EditText = findViewById(R.id.userhealth_et)
-        val healthNextBtn : Button = findViewById(R.id.userhealth_next_btn)
-
-        var healthInfo: String = ""
-
-        healthNextBtn.isEnabled = false
-
-        //input이 있을때만 버튼 활성화
-        healthEdit.addTextChangedListener(object: TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                healthInfo = healthEdit.text.toString()
-                healthNextBtn.isEnabled = healthInfo.isNotEmpty()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {}
-        })
-
-        healthNextBtn.setOnClickListener {
-            Toast.makeText(this, healthInfo, Toast.LENGTH_SHORT).show()
-            System.out.println("health info is "+healthInfo)
-            //Main 화면으로 전환
-            val intent = Intent(this@HealthActivity, StartActivity::class.java)
-            startActivity(intent)
-        }
+        init()
     }
 
-    fun init() {
-        binding.userhealthNextBtn.setOnClickListener{
-            System.out.println(binding.userhealthEt.text.toString())
+    private fun init() {
+        binding.apply {
+            userhealthNextBtn.isEnabled = false
+
+            //input이 있을때만 버튼 활성화
+            userhealthEt.addTextChangedListener(object: TextWatcher{
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    healthInfo = userhealthEt.text.toString()
+                    userhealthNextBtn.isEnabled = healthInfo.isNotEmpty()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+            })
+
+            userhealthNextBtn.setOnClickListener {
+                Toast.makeText(this@HealthActivity, healthInfo, Toast.LENGTH_SHORT).show()
+                System.out.println("health info is "+healthInfo)
+                //Main 화면으로 전환
+                val intent = Intent(this@HealthActivity, StartActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/wmn/firstRunActivity/UsernameActivity.kt
+++ app/src/main/java/com/example/wmn/firstRunActivity/UsernameActivity.kt
@@ -13,41 +13,40 @@ import com.example.wmn.databinding.ActivityUsernameBinding
 
 class UsernameActivity : AppCompatActivity() {
     lateinit var binding : ActivityUsernameBinding
+    lateinit var username :String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUsernameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val usernameEdit : EditText = findViewById(R.id.username_et)
-        val usernameNextBtn : Button = findViewById(R.id.username_next_btn)
-
-        var username: String = ""
-
-        usernameNextBtn.isEnabled = false
-
-        //input이 있을때만 버튼 활성화
-        usernameEdit.addTextChangedListener(object: TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                username = usernameEdit.text.toString()
-                usernameNextBtn.isEnabled = username.isNotEmpty()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {}
-        })
-
-        usernameNextBtn.setOnClickListener {
-            Toast.makeText(this, username, Toast.LENGTH_SHORT).show()
-            //좋아하는 음식조사로 전환
-            val intent = Intent(this@UsernameActivity, PreferenceActivity::class.java)
-            startActivity(intent)
-        }
+        init()
     }
 
-    fun init() {
-        binding.usernameNextBtn.setOnClickListener{
-            System.out.println(binding.usernameEt.text.toString())
+    private fun init() {
+        binding.apply {
+            usernameNextBtn.isEnabled = false
+
+            //input이 있을때만 버튼 활성화
+            usernameEt.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    username = usernameEt.text.toString()
+                    usernameNextBtn.isEnabled = username.isNotEmpty()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+            })
+
+            usernameNextBtn.setOnClickListener {
+                if (username != null) {
+                    Toast.makeText(this@UsernameActivity, username, Toast.LENGTH_SHORT).show()
+                    //좋아하는 음식조사로 전환
+                    val intent = Intent(this@UsernameActivity, PreferenceActivity::class.java)
+                    intent.putExtra("username", username)
+                    startActivity(intent)
+                }
+            }
         }
     }
 }
